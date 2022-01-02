2c4e23eacd2b6aae28c5bd08ca9c19784a3fa56c
Nan Huang <nan.huang@torstonetech.com>
2022-01-02 22:46:04 +0000

    Update Dockerfile for aarch64

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-docker build -t whzycrm . --build-arg BS_USER=$(id -un) --build-arg BS_UID=$(id -u) --build-arg BS_GROUP=$(id -gn) --build-arg BS_GID=$(id -g)
+set -e
+
+export GOBIN="$PWD/cmd/whzy_rest_api"
+(cd cmd/whzy_rest_api && go install)
+ARCH=$(arch)
+docker build -t whzycrm . --build-arg BS_USER=$(id -un) --build-arg BS_UID=$(id -u) --build-arg BS_GROUP=$(id -gn) --build-arg BS_GID=$(id -g) -f Dockerfile."$ARCH"
